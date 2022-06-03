ef93c097a1203739a78a1214e4a3379b86900038
NinivePat <ninivepat@gmail.com>
2022-06-02 21:29:00 -0500

    Finalice el proyecto, terminado de agregar cambios en pagar.php, pagofinalizado.php, agregue estilos.

---
+++ cancelar.php
@@ -0,0 +1,20 @@
+<?php
+include_once 'includes/templates/header.php'
+?>
+
+<section class="seccion contenedor">
+    <h2>Resumen Registro</h2>
+
+<?php
+ echo "<div class='resultado error'>";
+ echo "El pago esta cancelado";
+ echo "</div>";
+ ?>
+
+
+</section>
+<?php include_once 'includes/templates/footer.php';?>
+
+
+
+
